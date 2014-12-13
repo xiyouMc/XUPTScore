921e558842993478dd8471fd1c49805ba4928ce0
xiyouMc <694449212@qq.com>
2014-12-14 00:07:56 +0800

    the activity of addfriend

---
+++ src/com/xy/fy/main/AddFriendListActivity.java
@@ -0,0 +1,15 @@
+package com.xy.fy.main;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AddFriendListActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
+
+	
+}
