d7eca778624ebb146c06d2129dfe6b8b403efd97
xiyouMc <694449212@qq.com>
2015-01-01 23:04:46 +0800

    实现 好友申请和好友推荐按钮的切换 和 各自activity的编写

---
+++ src/com/xy/fy/main/FriendApplyActivity.java
@@ -0,0 +1,15 @@
+package com.xy.fy.main;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FriendApplyActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
+
+	
+}
---
+++ src/com/xy/fy/main/FriendRecommondActivity.java
@@ -0,0 +1,16 @@
+package com.xy.fy.main;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FriendRecommondActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_add_friend_recommand);
+	}
+
+	
+}
