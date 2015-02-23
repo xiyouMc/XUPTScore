a97f55e605859361dc730b84b19d4a2c1b6bcf13
xiyouMc <694449212@qq.com>
2015-02-23 21:14:35 +0800

    麻痹的，修复无法下载

--- src/com/mc/util/VersionUpdate.java
+++ src/com/mc/util/VersionUpdate.java
@@ -98,7 +98,7 @@ public class VersionUpdate {
 			@Override
 			public void onClick(DialogInterface dialog, int which) {
 				dialog.dismiss();
-				//showDownloadDialog();		
+				showDownloadDialog();		
 				//½øÈëä¯ÀÀÆ÷ÏÂÔØ
 			}
 		});
